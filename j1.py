f7b059749f6387e02bc516e9f43cd4d3dadbe873
Yerassyl005 <157362754+Yerassyl005@users.noreply.github.com>
2024-02-24 21:47:00 +0600

    Update j1.py

--- j1.py
+++ j1.py
@@ -1,5 +1,24 @@
 from json import *
 
-f = open("sample-data.json")
+file_path = "C:/Users/Home/Desktop/PyLab4/json/sample-data.json"
 
-data = load(f)
+try:
+    with open(file_path) as f:
+        data = load(f)
+        print("Interface Status\n================================================================================\nDN                                                 Description           Speed    MTU  \n-------------------------------------------------- --------------------  ------  ------")
+        for x in data["imdata"]:
+            dn = x["l1PhysIf"]["attributes"]["dn"]
+            while len(dn) < 48:
+                dn += " "
+            descr = x["l1PhysIf"]["attributes"]["descr"]
+            while len(descr) < 22:
+                descr += " "
+            speed = x["l1PhysIf"]["attributes"]["speed"]
+            while len(speed) < 9:
+                speed += " "
+            mtu = x["l1PhysIf"]["attributes"]["mtu"]
+            print(dn, descr, speed, mtu)
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
