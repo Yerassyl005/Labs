b1cb9dab97149736d4c2e1e17b4542e21593b2ed
Yerassyl005 <157362754+Yerassyl005@users.noreply.github.com>
2024-02-24 21:48:30 +0600

    Update Json.py

--- Json.py
+++ Json.py
@@ -1,14 +1,1 @@
-from json import *
 
-j = open('sample-data.json')
-data = load(j)
-print('Interface Status\n================================================================================\nDN                                                 Description           Speed    MTU  \n-------------------------------------------------- --------------------  ------  ------')
-for x in data['imdata']:
-    dn = x['l1PhysIf']['attributes']['dn']
-    while len(dn)<48: dn+=' '
-    descr = x['l1PhysIf']['attributes']['descr']
-    while len(descr)<22: descr+=' '
-    speed = x['l1PhysIf']['attributes']['speed']
-    while len(speed)<9: speed+=' '
-    mtu = x['l1PhysIf']['attributes']['mtu']
-    print(dn, descr, speed, mtu)
